e4777a2d5e70afc5d368abe8f31cf62029c33a96
Your Name <you@example.com>
2021-04-08 14:01:54 +0800

    基本语法-2

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 var x = 1;
+console.log(x)
+
+
+var x = 1; var y = 2; 
+
+
+if (2 > 1) {
+    x = 1;
+    y = 2;
+    z = 3;
+}
+
+if (2 > 1) {
+    x = 1;
+    y = 2;
+    z = 3;
+    if (x < y) {
+        console.log(555)
+        z = 4;
+    }
+    if (x > y) {
+        z = 5;
+    }
+}
